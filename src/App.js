a58589b4cc46dbe823290ccea1b0ba2ec14a383a
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-23 12:30:01 +0200

    added generating pargraphs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
@@ -26,14 +33,9 @@ function App() {
         </button>
       </form>
       <article className='lorem-text'>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione,
-          magnam?
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione,
-          magnam?
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
