096773aa8db3260c597e1cf4873aef456b623471
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-24 16:17:21 +0200

    added opening and closing modal and sidebar with contex

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className='sidebar-toggle'>
+      <button onClick={openSidebar} className='sidebar-toggle'>
         <FaBars />
       </button>
-      <button className='btn'>show modal</button>
+      <button onClick={openModal} className='btn'>
+        show modal
+      </button>
     </main>
   );
 };
