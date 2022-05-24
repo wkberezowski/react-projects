88a2969b8540d46054597015d1f9dc3dc7ee6dd1
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-24 21:25:22 +0200

    added sidebar and submenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,26 +5,37 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  const displaySubmenu = (e) => {
+    openSubmenu();
+  };
   return (
     <nav className='nav'>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} className='nav-logo' alt='stripe' />
-          <button className='btn toggle-btn' onClick={openSidebar}></button>
-          <FaBars />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
         </div>
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
-        <button className='btn signin-btn'></button>
+        <button className='btn signin-btn'>Sign In</button>
       </div>
     </nav>
   );
