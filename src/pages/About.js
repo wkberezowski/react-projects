d794b1c2cfdc044587849eef674f3542c57d3db1
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-25 16:28:50 +0200

    added navbar, about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum corporis
+        vero voluptate voluptas veniam reprehenderit asperiores expedita
+        laboriosam officia harum aspernatur perferendis deserunt consectetur
+        possimus, modi beatae architecto doloremque quaerat natus necessitatibus
+        autem doloribus exercitationem neque ipsum! Repellendus, quam quia?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
