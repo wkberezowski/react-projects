4e323bee5f3f536e43ac1f3f425b461b277b2c75
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-26 13:18:45 +0200

    added search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef('');
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
